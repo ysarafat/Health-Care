4346275aa83ffca583b3c8f2d5fa235482779c3e
Yeasir Arafat <ysarafat.bd@gmail.com>
2024-04-24 17:03:18 +0600

    register patient

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,7 @@
+"use client";
 import assets from "@/assets";
+import { registerPatient } from "@/services/actions/registerPatient";
+import { modifyPayload } from "@/utils/modifyPayload";
 import {
   Box,
   Button,
@@ -10,8 +13,40 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
 
+type Inputs = {
+  password: string;
+  patient: {
+    name: string;
+    email: string;
+    contactNumber: string;
+    address: string;
+  };
+};
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const router = useRouter();
+  const onSubmit: SubmitHandler<Inputs> = async (values) => {
+    const data = modifyPayload(values);
+    try {
+      const res = await registerPatient(data);
+      if (res?.data?.id) {
+        toast.success(res.message);
+        router.push("/login");
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Container>
       <Stack
@@ -25,7 +60,6 @@ const Register = () => {
           sx={{
             maxWidth: 600,
             width: "100%",
-
             boxShadow: 1,
             borderRadius: 1,
             p: 4,
@@ -52,60 +86,67 @@ const Register = () => {
             </Box>
           </Stack>
           <Box>
-            <Grid container spacing={2} my={1}>
-              <Grid item md={12}>
-                <TextField
-                  id="outlined-basic"
-                  label="Name"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={12}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.name", { required: true })}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.email", { required: true })}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("password")}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Contact Number"
+                    type="tel"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.contactNumber", { required: true })}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Address"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.address")}
+                  />
+                </Grid>
               </Grid>
-              <Grid item md={6}>
-                <TextField
-                  id="outlined-basic"
-                  label="Email"
-                  type="email"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
-              </Grid>
-              <Grid item md={6}>
-                <TextField
-                  id="outlined-basic"
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
-              </Grid>
-              <Grid item md={6}>
-                <TextField
-                  id="outlined-basic"
-                  label="Contact Number"
-                  type="tel"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
-              </Grid>
-              <Grid item md={6}>
-                <TextField
-                  id="outlined-basic"
-                  label="Address"
-                  type="text"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                />
-              </Grid>
-            </Grid>
-            <Button fullWidth={true} sx={{ margin: "15px 0" }}>
-              Register
-            </Button>
+              <Button fullWidth={true} sx={{ margin: "15px 0" }} type="submit">
+                Register
+              </Button>
+            </form>
             <Typography
               component="p"
               fontWeight={300}
